06cd09edac192aa1e38095556e740c76a0b11068
Ann M Hagan <39930638+anniepuy@users.noreply.github.com>
2023-09-09 16:15:08 -0500

    adding conditional view

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -12,13 +12,13 @@ function ListGroup () {
         'Abs'
     ];
 
-  
     //Turn the array into a list
     return (
         <>
             <h1>Body Part</h1>
+            {bodypart.length === 0 && <p>No body part selected</p>}
             <ul className="list-group">
-            
+                
                 {bodypart.map((bodypart) => (<li key = {bodypart}>{bodypart}</li>))}  
             </ul>
         </>
