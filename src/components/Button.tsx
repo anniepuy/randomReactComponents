6dc0a0714ec6deced2fb85695ddc9ecd4b759706
Ann M Hagan <39930638+anniepuy@users.noreply.github.com>
2023-09-09 19:19:17 -0500

    setting color with different options

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,7 +2,7 @@
 interface Props {
     children: string;
     onClick: () => void;
-    color?: string;
+    color?: 'primary' | 'secondary' | 'danger' | 'success';
 }
 
 const Button= ({children, onClick, color = 'primary'}: Props) => {
