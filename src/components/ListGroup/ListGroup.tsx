6739ddc02cf815ecb9ee9d5693bb847f91c4cc97
Ann M Hagan <39930638+anniepuy@users.noreply.github.com>
2023-09-09 22:50:26 -0500

    import styled components

--- src/components/ListGroup/ListGroup.tsx
+++ src/components/ListGroup/ListGroup.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import styles from './ListGroup.css';
+import './ListGroup.css';
+import styled from 'styled-components';
 
 function ListGroup () {
     const bodypart = [
